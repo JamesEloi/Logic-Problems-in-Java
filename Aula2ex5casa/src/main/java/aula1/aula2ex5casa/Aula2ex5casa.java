75dfe7c88edd59a60ddc24a16458302a677e7a5d
Jhames Eloi Lima <high7j@gmail.com>
2025-02-22 23:34:23 -0300

    Logic Problems in Java
    Here are some logic problems in Java that I found in my computer's folders. I found some of them, but there are still others missing."

---
+++ Aula2ex5casa/src/main/java/aula1/aula2ex5casa/Aula2ex5casa.java
@@ -0,0 +1,10 @@
+package aula1.aula2ex5casa;
+public class Aula2ex5casa {
+// raio = 5cm
+    public static void main(String[] args) {
+        float raio = 5f;
+        final double pi = 3.14159;
+        double c = (2*pi) * raio;
+        System.out.println("A circunferancia mede: " + c + "m");
+    }
+}
